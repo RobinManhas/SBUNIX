96894ee01a2c6d9dad4b5f9fc5930bb75936063a
robinmanhas <robin.manhas@github.com>
2017-11-12 15:23:09 -0500

    Complete remodification of kernel thread and context switch logic
    RM: Complete remodification of kernel thread and context switch logic based on prof comment. Earlier we just recalled a new function. However we had to actually continue execution of previous thread where we left from

--- sys/procmgr.c
+++ sys/procmgr.c
@@ -8,78 +8,59 @@
 #include <sys/gdt.h>
 #include <sys/vmm.h>
 #include <sys/pmm.h>
-#include <sys/procmgr.h>
 #include <sys/idt.h>
 
-task_struct *t1,*t2;
-int f1flag=0;
+
+task_struct *t0,*t1,*t2;
+
 void func1()
 {
-    while(1)
-    {
-        if(f1flag<10)
-        {
-            f1flag += 1;
-            kprintf("I'm thread #1\n");
-            switch_to(t1,t2);
-        }
-        else
-        {
-            kprintf("initing idt in thread 1\n");
-            init_idt();
-            init_irq();
-            init_timer();
-            init_keyboard();
-            __asm__ ("sti");
-            while(1);
-        }
-    }
+    kprintf("In thread #1\n");
+    switch_to(t1,t2);
+    kprintf("Returning from switchTo in thread #1\n");
+    init_idt();
+    init_irq();
+    init_timer();
+    init_keyboard();
+    __asm__ ("sti");
+    while(1);
+
 }
-int f2flag = 0;
+
 void func2()
 {
-    while(1)
-    {
-        if(f2flag<10)
-        {
-            f2flag += 1;
-            kprintf("I'm thread #2\n");
-            switch_to(t2,t1);
-        }
-        else
-        {
-            kprintf("initing idt in thread 2\n");
-            init_idt();
-            init_irq();
-            init_timer();
-            init_keyboard();
-            __asm__ ("sti");
-            while(1);
-        }
-    }
+    kprintf("In thread #2\n");
+    switch_to(t2,t1);
+    kprintf("Returning from switchTo in thread #2\n");
+    while(1);
+
 }
 
 void switch_to(task_struct *current, task_struct *next)
 {
-    void *updatedRSP = (void*)next->rsp;
-    __asm volatile("movq %%rsp, %0":: "r"(&(current->rsp)));
-    __asm volatile("movq %0, %%rsp":: "r"(next->rsp));
-    __asm volatile("movq %0, %%rax":: "r"(next->rip));
-    __asm volatile("pushq %%rax"::);
-    __asm volatile("movq %%rsp, %0":: "r"(&(updatedRSP)));
-    set_tss_rsp(updatedRSP);
-    __asm volatile("ret");
+    __asm__ __volatile__("movq %%rsp, %0":"=r"(current->rsp));
+    current->rsp+=8;
+    __asm__ __volatile__("movq %0, %%rsp":: "r"(next->rsp));
+    set_tss_rsp((void*)next->rsp);
+    __asm__ __volatile__("ret");
 }
+
+
 
 void threadInit(){
     kprintf("In thread Init, size of task struct: %d\n", sizeof(task_struct));
+    t0 = (task_struct*)kmalloc();
     t1 = (task_struct*)kmalloc();
     t2 = (task_struct*)kmalloc();
+
+    t1->stack[499] = (uint64_t)&func1;
+    t2->stack[499] = (uint64_t)&func2;
 
     t1->rsp = (uint64_t)&t1->stack[499];
     t2->rsp = (uint64_t)&t2->stack[499];
 
     t1->rip = (uint64_t)&func1;
     t2->rip = (uint64_t)&func2;
-    switch_to(t1,t2);
+
+    switch_to(t0,t1);
 }
